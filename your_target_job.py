49c9cab5dc40b7834c3016a9d1ed4c86f18a1ea6
Jens Bender <jens.bender.001@gmail.com>
2023-06-12 08:52:56 +0200

    create your_target_job.py with job_decription_url and motivation

---
+++ your_target_job.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 10:43:57 2023
+
+@author: Jens
+"""
+
+job_description_url = "URL_zur_Stellenbeschreibung_hier_einfügen"
+motivation = "Deine_Motivation_für_die_Stelle_hier_einfügen"
