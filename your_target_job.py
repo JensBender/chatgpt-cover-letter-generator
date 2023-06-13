f66d846ee129c60fcc6e9557bd05eb4ac2fb4aad
Jens Bender <jens.bender.001@gmail.com>
2023-06-13 12:00:55 +0200

    delete file author and creation date in your_target_job.py

--- your_target_job.py
+++ your_target_job.py
@@ -1,9 +1,2 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  1 10:43:57 2023
-
-@author: Jens
-"""
-
 job_description_url = "URL_zur_Stellenbeschreibung_hier_einfügen"
 motivation = "Deine_Motivation_für_die_Stelle_hier_einfügen"
