1bd1467e0ed0b9d5de7ef462fe6044c169b646b5
Jens Bender <jens.bender.001@gmail.com>
2024-10-18 11:20:03 +0200

    In your_secrets.py, change education from single line string to multi line string and add line breaks

--- your_secrets.py
+++ your_secrets.py
@@ -6,12 +6,18 @@ name = "Your_name_here"
 address = "Your_address_here"
 phone = "Your_phone_number_here"
 email = "Your_email_here"
-education = "Describe_your_education_here"
+
+education = """
+Describe_your_education_here
+"""
+
 work_experience = """
 Describe_your_work_experience_here
 """
+
 skills = """
 Describe_your_skills_here
 """
+
 salary_expectations = "Your_salary_expectations_here"
 possible_start_date = "Your_possible_start_date_here"
