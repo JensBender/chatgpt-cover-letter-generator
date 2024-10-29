f7da5ee7d42f5d2473db699e36b7ed5db4477c00
Jens Bender <jens.bender.001@gmail.com>
2024-10-29 13:53:51 +0100

    In your_information.py, restructure the information inputs to improve readability

--- your_information.py
+++ your_information.py
@@ -1,12 +1,7 @@
-# OpenAI API
+# OpenAI API key
 OPENAI_API_KEY = "Your_OPENAI_API_key_here"
 
-# Cover letter input
-name = "Your_name_here"
-address = "Your_address_here"
-phone = "Your_phone_number_here"
-email = "Your_email_here"
-
+# Professional background
 education = """
 Describe_your_education_here
 """
@@ -19,24 +14,26 @@ skills = """
 Describe_your_skills_here
 """
 
+# Personal information
+name = "Your_name_here"
+address = "Your_address_here"
+phone = "Your_phone_number_here"
+email = "Your_email_here"
 salary_expectations = "Your_salary_expectations_here"
 possible_start_date = "Your_possible_start_date_here"
 
 # Target job details
 job_description_url = "URL_of_job_posting_here"
-motivation = "Your_motivation_for_the_job_here"
+motivation = """
+Your_motivation_for_the_job_here
+"""
 
 
 # =========================
 #         EXAMPLE       
 # =========================
 
-# # Cover letter input
-# name = "John Doe"
-# address = "123 Main Street, Anytown, USA"
-# phone = "+1 (555) 123-4567"
-# email = "john.doe@email.com"
-
+# # Professional background
 # education = """
 # Bachelor of Science in Computer Science, XYZ University, Anytown, USA (2015-2019).
 # Master of Science in Machine Learning, ABC University, Somewhere City, USA (2020-2022)                                                                       
@@ -59,6 +56,11 @@ motivation = "Your_motivation_for_the_job_here"
 # Cloud Platforms: AWS, Google Cloud Platform
 # """
 
+# # Personal information
+# name = "John Doe"
+# address = "123 Main Street, Anytown, USA"
+# phone = "+1 (555) 123-4567"
+# email = "john.doe@email.com"
 # salary_expectations = "$100,000 - $120,000 per year"
 # possible_start_date = "2023-10-01"
 
